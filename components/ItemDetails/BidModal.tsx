5005bfbd036f9f2565fe67735274394489af1573
developerspack <developerspack23@gmail.com>
2023-12-25 01:42:57 +0300

    4th day

--- components/ItemDetails/BidModal.tsx
+++ components/ItemDetails/BidModal.tsx
@@ -1,4 +1,18 @@
-const BidModal = () => {
+interface BidModalProps {
+  open: boolean;
+  setOpen: (value: boolean) => void;
+  id: string;
+  name: string;
+  startingPrice: number;
+}
+
+const BidModal = ({
+  id,
+  name,
+  startingPrice,
+  open,
+  setOpen,
+}: BidModalProps) => {
   return <div>BidModal</div>;
 };
 
