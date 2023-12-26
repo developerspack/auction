ea2e8fee9cc1546b6feb687ac23754a3884807af
developerspack <developerspack23@gmail.com>
2023-12-27 00:27:48 +0300

    6th day

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -8,9 +8,11 @@ import { FetchCollection, FetchDocument } from "@/Hooks/Hooks";
 import { Separator } from "@/components/ui/separator";
 import OpenBids from "./OpenBids";
 import EmailModal from "./EmailModal";
+import { useUserStore } from "@/store/user";
 
 const Profile = ({ userId }: { userId: string }) => {
   const [modalopen, setModalOpen] = useState(false);
+  const { user } = useUserStore();
 
   const { document } = FetchDocument("users", userId);
   const { data } = FetchCollection("items", userId, "userId");
@@ -59,16 +61,15 @@ const Profile = ({ userId }: { userId: string }) => {
                 </div>
                 <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
                   <div className="py-6 px-3 mt-32 sm:mt-0">
-                    {/* {user.id === document.id && (
-
-                    )} */}
-                    <button
-                      className="bg-pink-500 active:bg-pink-600 uppercase font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150 inline-flex gap-2"
-                      onClick={() => setModalOpen(true)}
-                    >
-                      <SiMinutemailer className="text-blue-500 h-5 w-5" />
-                      <span>Email me</span>
-                    </button>
+                    {user.id !== document.id && (
+                      <button
+                        className="bg-pink-500 active:bg-pink-600 uppercase font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150 inline-flex gap-2"
+                        onClick={() => setModalOpen(true)}
+                      >
+                        <SiMinutemailer className="text-blue-500 h-5 w-5" />
+                        <span>Email me</span>
+                      </button>
+                    )}
                   </div>
                 </div>
                 <div className="w-full lg:w-4/12 px-4 lg:order-1 text-white">
