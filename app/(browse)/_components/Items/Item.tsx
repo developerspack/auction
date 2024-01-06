8736c2decde36242917afd67bbc662af7c30f9ec
developerspack <developerspack23@gmail.com>
2024-01-06 18:24:37 +0300

    update

--- app/(browse)/_components/Items/Item.tsx
+++ app/(browse)/_components/Items/Item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { ItemCard, ResultCardSkeleton } from "./ItemCard";
 import { FetchCollection } from "@/Hooks/Hooks";
 
@@ -14,32 +15,38 @@ export const Item = () => {
   });
 
   return (
-    <div>
-      <h2 className="text-lg font-semibold mb-4">
-        Items we think you&apos;ll like
-      </h2>
-      {NonExpiredItems.length === 0 && (
-        <div className="text-muted-foreground text-sm">No Items found.</div>
-      )}
+    <>
       {loading ? (
         <ResultsSkeleton />
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-2">
-          {NonExpiredItems.map((result: itemProps) => (
-            <ItemCard key={result.id} data={result} />
-          ))}
-        </div>
+        <>
+          <h2 className="text-lg font-semibold mb-4">
+            Items we think you&apos;ll like
+          </h2>
+          {NonExpiredItems.length === 0 && (
+            <div className="text-muted-foreground text-sm">No Items found.</div>
+          )}
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-2">
+            {NonExpiredItems.map((result: itemProps) => (
+              <ItemCard key={result.id} data={result} />
+            ))}
+          </div>
+        </>
       )}
-    </div>
+    </>
   );
 };
 
 export const ResultsSkeleton = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {[...Array(4)].map((_, i) => (
-        <ResultCardSkeleton key={i} />
-      ))}
-    </div>
+    <>
+      <Skeleton className="h-8 w-[290px] mb-4" />
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {[...Array(4)].map((_, i) => (
+          <ResultCardSkeleton key={i} />
+        ))}
+      </div>
+    </>
   );
 };
